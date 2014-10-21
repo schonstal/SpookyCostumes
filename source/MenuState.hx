d7f533dd9787e0a6c8dad393a72f6b10f320fba6
Josh Schonstal <josh@schonstal.org>
2014-10-20 23:31:40 -0700

    Annoying scrolling

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -5,6 +5,7 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxGradient;
 
 /**
  * A FlxState which can be used for the game's menu.
@@ -14,6 +15,10 @@ class MenuState extends FlxState
   override public function create():Void {
     super.create();
     FlxG.mouse.useSystemCursor = true;
+    add(FlxGradient.createGradientFlxSprite(FlxG.width,
+                                            FlxG.height,
+                                            [0xff43277c, 0xff130b22]));
+    add(new BackgroundGroup());
   }
   
   override public function destroy():Void {
